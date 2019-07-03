ea4380afc1c48722bab0bd4ba2ef294a11f4c823
Robley <robleyadrian@gmail.com>
2019-07-03 12:35:35 +0300

    Initial commit.

---
+++ src/test/java/com/stackabuse/hibernatedemo/HibernateDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stackabuse.hibernatedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HibernateDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
